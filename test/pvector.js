7e3c48de176a703eccf9d65d1ea482da4d70efb1
Makio135 <makio135@gmail.com>
2016-04-20 12:56:23 +0200

    fixing random2D and random3D tests

--- test/pvector.js
+++ test/pvector.js
@@ -1,205 +1,148 @@
-var expect = require('chai').expect;
-var PVector = require('../index');
+var expect = require( 'chai' ).expect;
+var PVector = require( '../index' );
 
 var EPSILON = 0.0001;
 
-describe('static methods', function () {
+describe( 'static methods', function() {
 
-    describe('new PVector', function () {
-        var x, y, z, vec1, vec2, vec3;
+    describe( 'new PVector', function() {
+        var x, y, z;
+        var vec1a, vec1b, vec2a, vec2b, vec3a, vec3b, vec4a, vec4b;
 
-        before(function () {
+        before( function() {
             x = 100;
             y = 200;
             z = 300;
-            vec1 = new PVector(x, y);
-            vec2 = new PVector(x, y, z);
-            vec3 = PVector(x, y);
-        });
 
-        it('should be an instance of PVector', function () {
-            expect(vec1).to.be.an.instanceof(PVector);
-            expect(vec2).to.be.an.instanceof(PVector);
-            expect(vec3).to.be.an.instanceof(PVector);
-        });
+            vec1a = new PVector( x, y );
+            vec1b = new PVector( x, y, z );
 
-        it('should have axis properties', function () {
-            expect(vec1).to.have.property('x', x);
-            expect(vec1).to.have.property('y', y);
-            expect(vec1).to.have.property('z', 0);
+            // Use Constructor without the `new` keyword
+            vec2a = PVector( x, y );
+            vec2b = PVector( x, y, z );
 
-            expect(vec2).to.have.property('x', x);
-            expect(vec2).to.have.property('y', y);
-            expect(vec2).to.have.property('z', z);
+            // Create a new vector from an array
+            vec3a = PVector( [ x, y ] );
+            vec3b = PVector( [ x, y, z ] );
 
-            expect(vec3).to.have.property('x', x);
-            expect(vec3).to.have.property('y', y);
-            expect(vec3).to.have.property('z', 0);
-        });
-    });
+            // Create a new vector from an object
+            vec4a = PVector( {
+                x: x,
+                y: y
+            } );
+            vec4b = PVector( {
+                x: x,
+                y: y,
+                z: z
+            } );
+        } );
 
-    describe('#fromAngle()', function () {
+        it( 'should be an instance of PVector', function() {
+            expect( vec1a ).to.be.an.instanceof( PVector );
+            expect( vec1b ).to.be.an.instanceof( PVector );
+            expect( vec2a ).to.be.an.instanceof( PVector );
+            expect( vec2b ).to.be.an.instanceof( PVector );
+            expect( vec3a ).to.be.an.instanceof( PVector );
+            expect( vec3b ).to.be.an.instanceof( PVector );
+            expect( vec4a ).to.be.an.instanceof( PVector );
+            expect( vec4b ).to.be.an.instanceof( PVector );
+        } );
+
+        it( 'should have axis properties', function() {
+            expect( vec1a ).to.have.property( 'x', x );
+            expect( vec1a ).to.have.property( 'y', y );
+            expect( vec1a ).to.have.property( 'z', 0 );
+
+            expect( vec1b ).to.have.property( 'x', x );
+            expect( vec1b ).to.have.property( 'y', y );
+            expect( vec1b ).to.have.property( 'z', z );
+
+            expect( vec2a ).to.have.property( 'x', x );
+            expect( vec2a ).to.have.property( 'y', y );
+            expect( vec2a ).to.have.property( 'z', 0 );
+
+            expect( vec2b ).to.have.property( 'x', x );
+            expect( vec2b ).to.have.property( 'y', y );
+            expect( vec2b ).to.have.property( 'z', z );
+
+            expect( vec3a ).to.have.property( 'x', x );
+            expect( vec3a ).to.have.property( 'y', y );
+            expect( vec3a ).to.have.property( 'z', 0 );
+
+            expect( vec3b ).to.have.property( 'x', x );
+            expect( vec3b ).to.have.property( 'y', y );
+            expect( vec3b ).to.have.property( 'z', z );
+
+            expect( vec4a ).to.have.property(  'x', x  );
+            expect( vec4a ).to.have.property(  'y', y  );
+            expect( vec4a ).to.have.property(  'z', 0  );
+
+            expect( vec4b ).to.have.property(  'x', x  );
+            expect( vec4b ).to.have.property(  'y', y  );
+            expect( vec4b ).to.have.property(  'z', z  );
+        } );
+    } );
+
+    describe( '#fromAngle()', function() {
         var angle, vec;
 
-        before(function () {
+        before( function() {
             angle = Math.random() * 2 * Math.PI;
-            vec = PVector.fromAngle(angle);
-        });
+            vec = PVector.fromAngle( angle );
+        } );
 
-        it('should return an instance of PVector', function () {
-            expect(vec).to.be.an.instanceof(PVector);
-        });
+        it( 'should return an instance of PVector', function() {
+            expect( vec ).to.be.an.instanceof( PVector );
+        } );
 
-        it('should have axis from angle', function () {
+        it( 'should have axis from angle', function() {
             expect( Math.abs( vec.x - Math.cos( angle ) ) ).to.lte( EPSILON );
             expect( Math.abs( vec.y - Math.sin( angle ) ) ).to.lte( EPSILON );
-        });
-    });
+        } );
+    } );
 
-    /*
-    describe('#random2D()', function () {
-        var arr, vec;
+    describe( '#random2D()', function() {
+        var vec;
 
-        before(function () {
-            arr = [100, 200];
-            vec = Victor.fromArray(arr);
-        });
+        before( function() {
+            vec = PVector.random2D();
+        } );
 
-        it('should return an instance of Victor', function () {
-            expect(vec).to.be.an.instanceof(Victor);
-        });
+        it( 'should return an instance of PVector', function() {
+            expect( vec ).to.be.an.instanceof( PVector );
+        } );
 
-        it('should have axis from array', function () {
-            expect(vec).to.have.property('x', arr[0]);
-            expect(vec).to.have.property('y', arr[1]);
-        });
-    });
+        it( 'should have random x and y axis', function() {
+            expect( vec ).to.have.property( 'x' ).to.lte( 1 );
+            expect( vec ).to.have.property( 'y' ).to.lte( 1 );
+            expect( vec ).to.have.property( 'z' ).to.equal( 0 );
+        } );
 
-    describe('#random3D()', function () {
-        var arr, vec;
+        it( 'should have a magnitude of ~1', function() {
+            expect( Math.abs( 1 - Math.sqrt( vec.x * vec.x + vec.y * vec.y ) ) ).to.lte( EPSILON );
+        } );
+    } );
 
-        before(function () {
-            arr = [100, 200];
-            vec = Victor.fromArray(arr);
-        });
+    describe( '#random3D()', function() {
+        var vec;
 
-        it('should return an instance of Victor', function () {
-            expect(vec).to.be.an.instanceof(Victor);
-        });
+        before( function() {
+            vec = PVector.random3D();
+        } );
 
-        it('should have axis from array', function () {
-            expect(vec).to.have.property('x', arr[0]);
-            expect(vec).to.have.property('y', arr[1]);
-        });
-    });
+        it( 'should return an instance of PVector', function() {
+            expect( vec ).to.be.an.instanceof( PVector );
+        } );
 
-    describe('#dist()', function () {
-        var arr, vec;
+        it( 'should have random x, y and z axis', function() {
+            expect( vec ).to.have.property( 'x' ).to.lte( 1 );
+            expect( vec ).to.have.property( 'y' ).to.lte( 1 );
+            expect( vec ).to.have.property( 'z' ).to.lte( 1 );
+        } );
 
-        before(function () {
-            arr = [100, 200];
-            vec = Victor.fromArray(arr);
-        });
+        it( 'should have a magnitude of ~1', function() {
+            expect( Math.abs( 1 - Math.sqrt( vec.x * vec.x + vec.y * vec.y + vec.z * vec.z ) ) ).to.lte( EPSILON );
+        } );
+    } );
 
-        it('should return an instance of Victor', function () {
-            expect(vec).to.be.an.instanceof(Victor);
-        });
-
-        it('should have axis from array', function () {
-            expect(vec).to.have.property('x', arr[0]);
-            expect(vec).to.have.property('y', arr[1]);
-        });
-    });
-
-    describe('#dot()', function () {
-        var arr, vec;
-
-        before(function () {
-            arr = [100, 200];
-            vec = Victor.fromArray(arr);
-        });
-
-        it('should return an instance of Victor', function () {
-            expect(vec).to.be.an.instanceof(Victor);
-        });
-
-        it('should have axis from array', function () {
-            expect(vec).to.have.property('x', arr[0]);
-            expect(vec).to.have.property('y', arr[1]);
-        });
-    });
-
-    describe('#cross()', function () {
-        var arr, vec;
-
-        before(function () {
-            arr = [100, 200];
-            vec = Victor.fromArray(arr);
-        });
-
-        it('should return an instance of Victor', function () {
-            expect(vec).to.be.an.instanceof(Victor);
-        });
-
-        it('should have axis from array', function () {
-            expect(vec).to.have.property('x', arr[0]);
-            expect(vec).to.have.property('y', arr[1]);
-        });
-    });
-
-    describe('#sub()', function () {
-        var arr, vec;
-
-        before(function () {
-            arr = [100, 200];
-            vec = Victor.fromArray(arr);
-        });
-
-        it('should return an instance of Victor', function () {
-            expect(vec).to.be.an.instanceof(Victor);
-        });
-
-        it('should have axis from array', function () {
-            expect(vec).to.have.property('x', arr[0]);
-            expect(vec).to.have.property('y', arr[1]);
-        });
-    });
-
-    describe('#angleBetween()', function () {
-        var arr, vec;
-
-        before(function () {
-            arr = [100, 200];
-            vec = Victor.fromArray(arr);
-        });
-
-        it('should return an instance of Victor', function () {
-            expect(vec).to.be.an.instanceof(Victor);
-        });
-
-        it('should have axis from array', function () {
-            expect(vec).to.have.property('x', arr[0]);
-            expect(vec).to.have.property('y', arr[1]);
-        });
-    });
-
-    describe('#lerp()', function () {
-        var arr, vec;
-
-        before(function () {
-            arr = [100, 200];
-            vec = Victor.fromArray(arr);
-        });
-
-        it('should return an instance of Victor', function () {
-            expect(vec).to.be.an.instanceof(Victor);
-        });
-
-        it('should have axis from array', function () {
-            expect(vec).to.have.property('x', arr[0]);
-            expect(vec).to.have.property('y', arr[1]);
-        });
-    });
-    */
-
-});
+} );
